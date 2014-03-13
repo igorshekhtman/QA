f9b31844224e4f2f1c6da8102b73d118c786b7f3
nkrishna123 <nkrishna@apixio.com>
2014-03-13 13:17:56 -0700

    changed the enum.

--- dashboard/src/main/java/com/apixio/qa/hive/Constants.java
+++ dashboard/src/main/java/com/apixio/qa/hive/Constants.java
@@ -4,6 +4,49 @@ public class Constants
 {
     public static enum COMPONENTS
     {
-        COORDINATOR, DOCRECEIVER, OCRJOB, PARSERJOB, PERSISTJOB, JSON2TRACE
+        COORDINATOR 
+        {
+            public String getTableName()
+            {
+                return "coordinator";
+            }
+        }, 
+        DOCRECEIVER
+        {
+            public String getTableName()
+            {
+                return "docreceiver";
+            }
+        }, 
+        OCR
+        {
+            public String getTableName()
+            {
+                return "ocrjob";
+            }
+        }, 
+        PARSERJOB
+        {
+            public String getTableName()
+            {
+                return "parserjob";
+            }
+        }, 
+        PERSISTJOB
+        {
+            public String getTableName()
+            {
+                return "persistjob";
+            }
+        },
+        TRACE
+        {
+            public String getTableName()
+            {
+                return "json2trace";
+            }
+        };
+        
+        public abstract String getTableName();
     }
 }
