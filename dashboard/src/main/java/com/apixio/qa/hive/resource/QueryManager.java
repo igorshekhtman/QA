bb86413bd94375d337a0f54d6a4170a49266a29a
nkrishna123 <nkrishna@apixio.com>
2014-03-20 14:40:39 -0700

    structuring the output.

--- dashboard/src/main/java/com/apixio/qa/hive/resource/QueryManager.java
+++ dashboard/src/main/java/com/apixio/qa/hive/resource/QueryManager.java
@@ -1,17 +1,13 @@
 package com.apixio.qa.hive.resource;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.apixio.qa.hive.resource.generated.Queries.Group;
@@ -22,91 +18,182 @@ public class QueryManager
     private QueryHandler queryHandler;
     private String hiveAddress;
     private String outputDir;
-    
-    public static void main(String[] args) throws SQLException, JSONException, FileNotFoundException, IOException, ClassNotFoundException {
-    	String hiveAddress = "jdbc:hive2://184.169.209.24:10000";
-    	String outputDir = "C:\\alarocca_git\\QA\\dashboard\\assets\\reports\\json\\";
-    	QueryManager qm = new QueryManager(hiveAddress, outputDir);
-    	qm.processQueryGroup("completeness");
+
+    public static void main(String[] args) throws Exception
+    {
+        String hiveAddress = "jdbc:hive2://184.169.209.24:10000";
+        String outputDir = "C:/Users/nkrishna/Documents/apixio/docs/hive/";
+        QueryManager qm = new QueryManager(hiveAddress, outputDir);
+        qm.processQueryGroup("completeness");
     }
-    
+
     public QueryManager(String hiveAddress, String outputDir)
     {
-    	this.hiveAddress = hiveAddress;
-    	this.outputDir = outputDir;
+        this.hiveAddress = hiveAddress;
+        this.outputDir = outputDir;
         queryHandler = new QueryHandler();
     }
-    
-    public JSONObject processQueryGroup(String groupName) throws SQLException, JSONException, FileNotFoundException, IOException, ClassNotFoundException
+
+    public JSONObject processQueryGroup(String groupName) throws Exception
     {
         Group groupToRun = QueryConfig.getQueryGroupByName(groupName);
-        
+
         List<RunQuery> rQs = groupToRun.getRunQuery();
-        
+
         JSONObject processedGroup = null;
-        if (groupName.equalsIgnoreCase("completeness")) {
-        	processedGroup = processCompletenessGroup(rQs);
+        if (groupName.equalsIgnoreCase("completeness"))
+        {
+            processedGroup = processCompletenessGroup(rQs);
         }
         return processedGroup;
     }
-    
-    private JSONObject processCompletenessGroup(List<RunQuery> runQueries) throws SQLException, JSONException, FileNotFoundException, IOException, ClassNotFoundException
+
+    private JSONObject processCompletenessGroup(List<RunQuery> runQueries) throws Exception
     {
         JSONObject resultObj = null;
         if (runQueries != null)
         {
-            resultObj =  new JSONObject();
+            resultObj = new JSONObject();
             JSONObject orgDetails = new JSONObject();
-            resultObj.put("orgDetails", orgDetails);
+
             for (RunQuery rQ : runQueries)
             {
-            	List<JSONObject> results = null;
-            	File jsonFile = new File(outputDir + rQ.getName());
-            	
-            	if (jsonFile.exists()) { 
-            		Scanner scanner = new Scanner(jsonFile);
-            		results = new ArrayList<JSONObject>();
-            		while (scanner.hasNextLine()) {
-            			results.add(new JSONObject(scanner.nextLine()));
-            		}
-            		scanner.close();
-            	} else {
-	            	results = queryHandler.runQuery(hiveAddress, rQ);
-                    IOUtils.write(StringUtils.join(results, "\n"), new FileOutputStream(jsonFile));
-            	}
-            	
-                //TODO save the results in cache...
-            	if (rQ.getName().equalsIgnoreCase("archivedToS3_count"))
-            	{
-            	}
-            	else if (rQ.getName().equalsIgnoreCase("parse_count"))
+                List<JSONObject> results = null;
+                File jsonFile = new File(outputDir + rQ.getName());
+
+                if (jsonFile.exists())
                 {
-                    for (JSONObject result : results)
+                    Scanner scanner = new Scanner(jsonFile);
+                    results = new ArrayList<JSONObject>();
+                    while (scanner.hasNextLine())
                     {
-                    	String org_id = result.getString("org_id");
-                    	String status = result.getString("status");
-                    	String doc_count = result.getString("doc_count");
-                    	if (status.equals("success")) {
-                    		JSONObject orgObject = null;
-                    		if (orgDetails.has(org_id)) {
-                    			orgObject = orgDetails.getJSONObject(org_id);
-                    		} else {
-                    			orgObject = new JSONObject();
-                    			orgDetails.put(org_id, orgObject);
-                    			orgObject.put("org_id", org_id);
-                    		}
-                    		if (result.has("sent_to_persist")) {
-                    			orgObject.put("sent_to_persist", doc_count);
-                    		} else if (result.has("sent_to_ocr")) {
-                    			orgObject.put("sent_to_ocr", doc_count);                    			
-                    		}
-                    	}
+                        results.add(new JSONObject(scanner.nextLine()));
                     }
+                    scanner.close();
                 }
+                else
+                {
+                    results = queryHandler.runQuery(hiveAddress, rQ);
+                    IOUtils.write(StringUtils.join(results, "\n"), new FileOutputStream(jsonFile));
+                }
+
+                getOrgObject(results, rQ.getName(), orgDetails);
             }
+
+            resultObj.put("orgDetails", orgDetails);
         }
         System.out.println(resultObj);
-        return null;
+        return resultObj;
     }
-    
+
+    private void getOrgObject(List<JSONObject> results, String typeOfResults, JSONObject orgDetails) throws Exception
+    {
+        for (JSONObject result : results)
+        {
+            String org_id = result.getString("org_id");
+            String status = "NotKnown";
+            String errorMsg = "NotKnown";
+            
+            if (result.has("error_message"))
+                errorMsg = result.getString("error_message");
+            
+            Integer doc_count = result.getInt("doc_count");
+            
+            JSONObject orgObject = null;
+            
+            if (orgDetails.has(org_id))
+            {
+                orgObject = orgDetails.getJSONObject(org_id);
+            }
+            else
+            {
+                orgObject = new JSONObject();
+            }
+            
+            if (result.has("status"))
+                status = result.getString("status");
+            
+            if (status.equals("success"))
+            {
+                if (typeOfResults.equalsIgnoreCase("parse_count"))
+                {
+                    if (result.has("sent_to_persist"))
+                    {
+                        orgObject.put("sent_to_persist", doc_count);
+                    }
+                    else if (result.has("sent_to_ocr"))
+                    {
+                        orgObject.put("sent_to_ocr", doc_count);
+                    }
+                }
+                else if (typeOfResults.equalsIgnoreCase("persist_count"))
+                {
+                    orgObject.put("persist_success", doc_count);
+                }
+                else if (typeOfResults.equalsIgnoreCase("ocr_count"))
+                {
+                    orgObject.put("ocr_success", doc_count);
+                }
+                else if (typeOfResults.equalsIgnoreCase("verified_count"))
+                {
+                    orgObject.put("persist_verified", doc_count);
+                }
+                else if (typeOfResults.equalsIgnoreCase("archivedToS3_count"))
+                {
+                    orgObject.put("archive_success", doc_count);
+                }
+            }
+            else if (status.equals("error"))
+            {
+                JSONObject errorObject = new JSONObject();
+                
+                if (typeOfResults.equalsIgnoreCase("parse_count"))
+                {
+                    if (orgObject.has("parse_errors"))
+                    {
+                        errorObject = (JSONObject)orgObject.get("parse_errors");
+                    }
+                    errorObject.put(errorMsg, errorObject.has(errorMsg)?
+                            (((Integer)errorObject.get(errorMsg))+doc_count):doc_count);
+                    
+                    orgObject.put("parse_errors", errorObject);
+                }
+                else if (typeOfResults.equalsIgnoreCase("persist_count"))
+                {
+                    if (orgObject.has("persist_errors"))
+                    {
+                        errorObject = (JSONObject)orgObject.get("persist_errors");
+                    }
+                    errorObject.put(errorMsg, errorObject.has(errorMsg)?
+                            (((Integer)errorObject.get(errorMsg))+doc_count):doc_count);
+                    
+                    orgObject.put("persist_errors", errorObject);
+                }
+                else if (typeOfResults.equalsIgnoreCase("ocr_count"))
+                {
+                    if (orgObject.has("ocr_errors"))
+                    {
+                        errorObject = (JSONObject)orgObject.get("ocr_errors");
+                    }
+                    errorObject.put(errorMsg, errorObject.has(errorMsg)?
+                            (((Integer)errorObject.get(errorMsg))+doc_count):doc_count);
+                    
+                    orgObject.put("ocr_errors", errorObject);
+                }
+                else if (typeOfResults.equalsIgnoreCase("archivedToS3_count"))
+                {
+                    if (orgObject.has("archive_errors"))
+                    {
+                        errorObject = (JSONObject)orgObject.get("archive_errors");
+                    }
+                    errorObject.put(errorMsg, errorObject.has(errorMsg)?
+                            (((Integer)errorObject.get(errorMsg))+doc_count):doc_count);
+                    
+                    orgObject.put("archive_errors", errorObject);
+                }
+            }
+            
+            orgDetails.put(org_id, orgObject);
+        }
+    }
 }
