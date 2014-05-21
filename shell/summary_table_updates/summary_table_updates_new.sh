f2acc93fa9ba132ce9153123407a5e3a2cbd96f2
nkrishna123 <nkrishna@apixio.com>
2014-05-21 09:53:32 -0700

    added care opt's login/logout summary tables

--- shell/summary_table_updates/summary_table_updates_new.sh
+++ shell/summary_table_updates/summary_table_updates_new.sh
@@ -861,6 +861,45 @@ staging_logs_careopt_epoch
 where get_json_object(line, '$.level') = 'ERROR' 
 and ($dateRange);
 
+
+insert overwrite table summary_careopt_login_staging partition (org_id, month, day)
+select
+get_json_object(line, '$.datestamp') as time,
+get_json_object(line, '$.login.username') as username,
+get_json_object(line, '$.login.userId') as user_id,
+get_json_object(line, '$.login.status') as status,
+get_json_object(line, '$.login.userAgent') as user_agent,
+get_json_object(line, '$.hostname') as hostname,
+cast(get_json_object(line, '$.login.millis') as int) as processTime,
+'login' as event,
+get_json_object(line, '$.login.remoteAddr') as remote_address,
+get_json_object(line, '$.login.orgId') as org_id,
+month,
+day
+from 
+staging_logs_careopt_epoch 
+where get_json_object(line, '$.login') is not null 
+and ($dateRange);
+
+insert overwrite table summary_careopt_login_staging partition (org_id, month, day)
+select
+get_json_object(line, '$.datestamp') as time,
+get_json_object(line, '$.logout.username') as username,
+get_json_object(line, '$.logout.userId') as user_id,
+get_json_object(line, '$.logout.status') as status,
+get_json_object(line, '$.logout.userAgent') as user_agent,
+get_json_object(line, '$.hostname') as hostname,
+cast(get_json_object(line, '$.logout.millis') as int) as processTime,
+'logout' as event,
+get_json_object(line, '$.logout.remoteAddr') as remote_address,
+get_json_object(line, '$.logout.orgId') as org_id,
+month,
+day
+from 
+staging_logs_careopt_epoch 
+where get_json_object(line, '$.logout') is not null 
+and ($dateRange);
+
 EOF
 
 chmod 777 update_summary.log
