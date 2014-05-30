bdf18656ae0c3ad0c30db39371b1657a3295069e
Igor Shekhtman <ishekhtman@apixio.com>
2014-05-30 16:21:12 -0700

    updated data files patient uuids and document uuids

--- gatling/user-files/simulations/basic/BasicExampleSimulation.scala
+++ gatling/user-files/simulations/basic/BasicExampleSimulation.scala
@@ -9,14 +9,18 @@ import assertions._
 
 class BasicExampleSimulation extends Simulation {
 
-  var patientuuid = "4e74c4a1-9af5-477f-a05a-e32e62c690aa"
-  var documentuuid = "9d9a36cd-28c3-4c05-a0c1-748de6b0d94e"
+  val patientuuid = "7390113c-25dc-4203-99f9-55525027dce6"
+  val documentuuid = "cac070a3-83fd-493c-86a4-2e2f5c57d56e"
   
-  val login = "54.193.204.86:8079"
+  //val ip = "54.193.204.86"
+  //val ip = "50.18.147.10"
+  val ip = "10.191.15.150"
+  
+  val login = ip + ":8079"
   val loginSubmit = login + "/uh/login"
-  val token = "54.193.204.86:8075/tokens"
-  val document = "54.193.204.86:8085/document"
-  val patient = "54.193.204.86:8085/patient"
+  val token = ip + ":8075/tokens"
+  val document = ip + ":8085/document"
+  val patient = ip + ":8085/patient"
 
   val thinkTime = 6 seconds
   val ok = 200
@@ -48,10 +52,10 @@ class BasicExampleSimulation extends Simulation {
           .param("email", "${username}")
           .param("password", "${password}")
           .check(headerRegex("Set-Cookie","tomswebapps=(.*);").saveAs("externalToken"))
-          //.check(headerRegex("Set-Cookie","tomswebapps=(.*);").transform(value => {
-          //  println("value = " + value.get)
-          //  value}
-          //))
+          .check(headerRegex("Set-Cookie","tomswebapps=(.*);").transform(value => {
+              println("value = " + value.get)
+              value}
+          ))
           .check(status.is(redirect)))
         .pause(thinkTime)
     }
@@ -60,10 +64,10 @@ class BasicExampleSimulation extends Simulation {
       http("Token exchange") // no think time
         .post(token)
         .header("Authorization", "Apixio ${externalToken}")
-        //.check(bodyString.transform(body => {
-        //  println("body = " + body.get)
-        //  body}
-        //))
+        .check(bodyString.transform(body => {
+          println("body = " + body.get)
+          body}
+        ))
         .check(jsonPath("$.token").saveAs("internalToken"))
         .check(status.is(created)))
     }
@@ -73,8 +77,8 @@ class BasicExampleSimulation extends Simulation {
       //param ("patientuuid", "${patient_uuid}")  
       http("Patient demographics")
         //.param("patientuuid", "${username}")
-        //.get(session => patient + "/4e74c4a1-9af5-477f-a05a-e32e62c690aa" + "/demographics")
-        .get("54.193.204.86:8085/patient/${patient_uuid}/demographics")
+        //.get(session => patient + "/" + patientuuid + "/demographics")
+        .get("10.191.15.150:8085/patient/${patient_uuid}/demographics")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
         // TODO: Add other data checks here, i.e. Content-Type and response length
@@ -84,22 +88,20 @@ class BasicExampleSimulation extends Simulation {
       feed(csv("document_uuids.csv"))
       .exec( 
       http("Document retrieval text")
-        .get("54.193.204.86:8085/document/${document_uuid}/text")
-        //f6bc5be1-fe39-4146-89d5-f3eb724d6591 - bad document
-        //2c10e935-ffe9-441e-a0b6-cf7aaa768063
-        //.get("54.193.204.86:8085/document/2c10e935-ffe9-441e-a0b6-cf7aaa768063/text")
+        .get("10.191.15.150:8085/document/${document_uuid}/text")
+        //.get(session => document + "/" + documentuuid + "/text")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
       .exec( 
       http("Document retrieval metadata")
-        .get("54.193.204.86:8085/document/${document_uuid}/metadata")
-        //.get("54.193.204.86:8085/document/2c10e935-ffe9-441e-a0b6-cf7aaa768063/metadata")
+        .get("10.191.15.150:8085/document/${document_uuid}/metadata")
+        //.get(session => document + "/" + documentuuid + "/metadata")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
       .exec( 
       http("Document retrieval file")
-        .get("54.193.204.86:8085/document/${document_uuid}/file")
-        //.get("54.193.204.86:8085/document/2c10e935-ffe9-441e-a0b6-cf7aaa768063/file")
+        .get("10.191.15.150:8085/document/${document_uuid}/file")
+        //.get(session => document + "/" + documentuuid + "/file")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
         // TODO: Add other data checks here, i.e. Content-Type and response length
@@ -109,12 +111,20 @@ class BasicExampleSimulation extends Simulation {
   
   // 30 users over 20 min
   // setUp(scn.inject(ramp(30 users) over (1200 seconds)))
+  // 30 users over 5 min
+  // setUp(scn.inject(ramp(30 users) over (300 seconds)))
   // 60 users over 10 min
-  setUp(scn.inject(ramp(60 users) over (600 seconds)))
+  // setUp(scn.inject(ramp(60 users) over (600 seconds)))
   // 30 users over 12 hours
-  //setUp(scn.inject(ramp(30 users) over (43200 seconds)))
+  // setUp(scn.inject(ramp(30 users) over (43200 seconds)))
+  // 60 users over 24 hours
+  // setUp(scn.inject(ramp(60 users) over (86400 seconds)))
+  // 30 users over 1 hour
+  // setUp(scn.inject(ramp(30 users) over (3600 seconds)))
   // 3 users over 20 seconds
-  //setUp(scn.inject(ramp(3 users) over (20 seconds)))
+  // setUp(scn.inject(ramp(3 users) over (20 seconds)))
+  // 11 users over 1 hour
+  setUp(scn.inject(ramp(11 users) over (3600 seconds)))
   
     .protocols(http.baseURL("http://").disableFollowRedirect) // Note we can't use this because we use multiple services
     .assertions( // TODO: Fix, these are the assertions that came with the sample code
