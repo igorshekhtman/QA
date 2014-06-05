7e36c4b2d5c0c2fb8538c82dc729bf9afa5040de
nkrishna123 <nkrishna@apixio.com>
2014-06-04 17:18:14 -0700

    corrected syntax errors.

--- shell/summary_table_updates/logtraffic_summary_updates.sh
+++ shell/summary_table_updates/logtraffic_summary_updates.sh
@@ -39,7 +39,7 @@ echo " "
 envs="staging production"
 appNames="persistJob json2trace parserJob coordinator ocrJob docreceiver careopt"
 
-for env in envs
+for env in $envs
 do
 
 environment="_"$env
@@ -48,7 +48,7 @@ then
   environment = ""
 fi
 
-for t in appNames
+for t in $appNames
 do
 
 /usr/bin/hive --service beeline -u jdbc:hive2://10.196.47.205:10000 -n hive -d org.apache.hive.jdbc.HiveDriver  >> update_summary.log   << EOF
