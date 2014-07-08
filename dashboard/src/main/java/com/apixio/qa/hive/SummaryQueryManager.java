1094ec354c2afa74eba2f8bbf51484638a55eceb
Lance Norskog <lnorskog@apixio.com>
2014-07-08 12:28:17 -0700

    Add year to Java querymanager

--- dashboard/src/main/java/com/apixio/qa/hive/SummaryQueryManager.java
+++ dashboard/src/main/java/com/apixio/qa/hive/SummaryQueryManager.java
@@ -33,24 +33,24 @@ public class SummaryQueryManager {
 		SummaryQuery docreceiverArchive = new SummaryQuery();
 		docreceiverArchive.setDescription("Documents Archived by Doc Receiver");
 		docreceiverArchive.setTableName("summary_docreceiver_archive");
-		docreceiverArchive.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.archive.afs.docid') as doc_id, get_json_object(line, '$.archive.afs.batchid') as batch_id, cast(get_json_object(line, '$.archive.afs.bytes') as int) as file_size, get_json_object(line, '$.archive.afs.status') as status, cast(get_json_object(line, '$.archive.afs.millis') as int) as archive_time, get_json_object(line, '$.message') as error_message, month, day, get_json_object(line, '$.archive.afs.orgid') as org_id from production_logs_docreceiver_epoch where get_json_object(line, '$.archive') is not null");
-		docreceiverArchive.setCreateStatement("create table summary_docreceiver_archive  (time string, doc_id string, batch_id string, file_size int, status string, archive_time int, error_message string) partitioned by (month string, day string, org_id string)");
+		docreceiverArchive.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.archive.afs.docid') as doc_id, get_json_object(line, '$.archive.afs.batchid') as batch_id, cast(get_json_object(line, '$.archive.afs.bytes') as int) as file_size, get_json_object(line, '$.archive.afs.status') as status, cast(get_json_object(line, '$.archive.afs.millis') as int) as archive_time, get_json_object(line, '$.message') as error_message, substr(get_json_object(line, '$.datestamp'),0,4) as year, month, day, get_json_object(line, '$.archive.afs.orgid') as org_id from production_logs_docreceiver_epoch where get_json_object(line, '$.archive') is not null");
+		docreceiverArchive.setCreateStatement("create table summary_docreceiver_archive  (time string, doc_id string, batch_id string, file_size int, status string, archive_time int, error_message string) partitioned by (year string, month string, day string, org_id string)");
 		
 		summaryQueries.put(docreceiverArchive.getTableName(), docreceiverArchive);
 		
 		SummaryQuery coordinatorStats = new SummaryQuery();
 		coordinatorStats.setDescription("Coordinator Stats");
 		coordinatorStats.setTableName("summary_coordinator_stats");
-		coordinatorStats.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.coordinator.stats') as stats_json, month, day from production_logs_coordinator_epoch where get_json_object(line, '$.coordinator.stats.parser.queuedCount') is not null");
-		coordinatorStats.setCreateStatement("create table summary_coordinator_stats (time string, stats_json string) partitioned by (month STRING, day STRING)");
+		coordinatorStats.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.coordinator.stats') as stats_json, substr(get_json_object(line, '$.datestamp'),0,4) as year, month, day from production_logs_coordinator_epoch where get_json_object(line, '$.coordinator.stats.parser.queuedCount') is not null");
+		coordinatorStats.setCreateStatement("create table summary_coordinator_stats (time string, stats_json string) partitioned by (year STRING, month STRING, day STRING)");
 		
 		summaryQueries.put(coordinatorStats.getTableName(), coordinatorStats);
 		
 		SummaryQuery coordinatorStatsStaging = new SummaryQuery();
 		coordinatorStats.setDescription("Coordinator Stats - Staging");
 		coordinatorStats.setTableName("summary_coordinator_stats_staging");
-		coordinatorStats.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.coordinator.stats') as stats_json, month, day from staging_logs_coordinator_epoch where get_json_object(line, '$.coordinator.stats.parser.queuedCount') is not null");
-		coordinatorStats.setCreateStatement("create table summary_coordinator_stats_staging (time string, stats_json string) partitioned by (month STRING, day STRING)");
+		coordinatorStats.setQuery("select get_json_object(line, '$.datestamp') as time, get_json_object(line, '$.coordinator.stats') as stats_json, substr(get_json_object(line, '$.datestamp'),0,4) as year, month, day from staging_logs_coordinator_epoch where get_json_object(line, '$.coordinator.stats.parser.queuedCount') is not null");
+		coordinatorStats.setCreateStatement("create table summary_coordinator_stats_staging (time string, stats_json string) partitioned by (year STRING, month STRING, day STRING)");
 		
 		summaryQueries.put(coordinatorStatsStaging.getTableName(), coordinatorStatsStaging);
 	}
