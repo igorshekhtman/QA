e5c9acc8f8515e41f9c98617cd0a5b035ff9df2b
Igor Shekhtman <ishekhtman@apixio.com>
2014-07-25 12:06:16 -0700

    added ability to run same scala load test script on staging and production enviroments

--- gatling/user-files/simulations/basic/BasicExampleSimulation.scala
+++ gatling/user-files/simulations/basic/BasicExampleSimulation.scala
@@ -9,12 +9,41 @@ import assertions._
 
 class BasicExampleSimulation extends Simulation {
 
+  //val environment = "Production"
+  val environment = "Staging"
+  
+  var ip = "54.193.231.8"
+  var userdatafn = "user_credentials_prod.csv"
+  var patientdatafn = "patient_uuids_prod.csv"
+  var documentdatafn = "document_uuids_prod.csv"
+  
+  //Auth Servers
+  //8075 - user
+  //8076 - token
+  //8085 - data
+  //Staging :
+    //internal - 10.191.15.150
+    //external - 50.18.147.10 - ssh -i ~/stagingauth.pem root@50.18.147.10
+  //Production :
+    //internal - 10.226.2.253
+    //external - 54.193.231.8 - ssh -i ~/production_microservices.pem root@54.193.231.8  
+  
   val patientuuid = "7390113c-25dc-4203-99f9-55525027dce6"
   val documentuuid = "cac070a3-83fd-493c-86a4-2e2f5c57d56e"
   
-  //val ip = "54.193.204.86"
-  //val ip = "50.18.147.10"
-  val ip = "10.191.15.150"
+  // Assigning vaiable values dependent on environment being Load Tested
+  if (environment == "Production") {
+	ip = "54.193.231.8"
+	userdatafn = "user_credentials_prod.csv"
+	patientdatafn = "patient_uuids_prod.csv"
+	documentdatafn = "document_uuids_prod.csv"
+  }
+  else {
+	ip = "50.18.147.10"
+	userdatafn = "user_credentials.csv"
+	patientdatafn = "patient_uuids.csv"
+	documentdatafn = "document_uuids.csv"  
+  }
   
   val login = ip + ":8079"
   val loginSubmit = login + "/uh/login"
@@ -45,7 +74,7 @@ class BasicExampleSimulation extends Simulation {
         .get(login)
         .check(status.is(ok)))
       .pause(thinkTime)
-      .feed(csv("user_credentials.csv"))
+      .feed(csv(userdatafn))
       .exec(
         http("Submit login")
           .post(loginSubmit)
@@ -72,35 +101,35 @@ class BasicExampleSimulation extends Simulation {
         .check(status.is(created)))
     }
     .group("Patient") {
-      feed(csv("patient_uuids.csv"))
+      feed(csv(patientdatafn))
       .exec(
       //param ("patientuuid", "${patient_uuid}")  
       http("Patient demographics")
         //.param("patientuuid", "${username}")
         //.get(session => patient + "/" + patientuuid + "/demographics")
-        .get("10.191.15.150:8085/patient/${patient_uuid}/demographics")
+        .get("50.18.147.10:8085/patient/${patient_uuid}/demographics")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
         // TODO: Add other data checks here, i.e. Content-Type and response length
       .pause(thinkTime)
     }
     .group("Document") {
-      feed(csv("document_uuids.csv"))
+      feed(csv(documentdatafn))
       .exec( 
       http("Document retrieval text")
-        .get("10.191.15.150:8085/document/${document_uuid}/text")
+        .get("50.18.147.10:8085/document/${document_uuid}/text")
         //.get(session => document + "/" + documentuuid + "/text")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
       .exec( 
       http("Document retrieval metadata")
-        .get("10.191.15.150:8085/document/${document_uuid}/metadata")
+        .get("50.18.147.10:8085/document/${document_uuid}/metadata")
         //.get(session => document + "/" + documentuuid + "/metadata")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
       .exec( 
       http("Document retrieval file")
-        .get("10.191.15.150:8085/document/${document_uuid}/file")
+        .get("50.18.147.10:8085/document/${document_uuid}/file")
         //.get(session => document + "/" + documentuuid + "/file")
         .header("Authorization", "Apixio ${internalToken}")
         .check(status.is(ok)))
@@ -112,7 +141,15 @@ class BasicExampleSimulation extends Simulation {
   // 30 users over 20 min
   // setUp(scn.inject(ramp(30 users) over (1200 seconds)))
   // 30 users over 5 min
-  // setUp(scn.inject(ramp(30 users) over (300 seconds)))
+  // rampRate(5 usersPerSec) to(30 usersPerSec) during(60 seconds)
+  // setUp(scn.inject(nothingFor(4 seconds), atOnce(10 users), ramp(10 users over 15 seconds), constantRate(30 usersPerSec) during (5 minutes)))
+  // 
+  // setUp(scn.inject(nothingFor(2 seconds), atOnce(2 users), ramp(11 users) over (5 minutes)))
+// setUp(scn.inject(nothingFor(4 seconds), atOnce(30 users), ramp(60 users) over(60 minutes))) 
+  
+  // 
+  // 
+  // 
   // 60 users over 10 min
   // setUp(scn.inject(ramp(60 users) over (600 seconds)))
   // 30 users over 12 hours
@@ -123,10 +160,13 @@ class BasicExampleSimulation extends Simulation {
   // setUp(scn.inject(ramp(30 users) over (3600 seconds)))
   // 3 users over 20 seconds
   // setUp(scn.inject(ramp(3 users) over (20 seconds)))
+  // 25 users over 120 seconds
+  setUp(scn.inject(ramp(25 users) over (120 seconds)))
   // 11 users over 1 hour
   // setUp(scn.inject(ramp(11 users) over (3600 seconds)))
   // 60 users over 1 hour
-  setUp(scn.inject(ramp(60 users) over (3600 seconds)))
+  // setUp(scn.inject(ramp(60 users) over (3600 seconds)))
+    
   
     .protocols(http.baseURL("http://").disableFollowRedirect) // Note we can't use this because we use multiple services
     .assertions( // TODO: Fix, these are the assertions that came with the sample code
