45abc39029ee3a2c50e4d4f9e72df14bf111f8b8
Igor Shekhtman <ishekhtman@apixio.com>
2014-09-08 09:06:34 -0700

    added data orchestrator to logs traffic job

--- shell/summary_table_updates/logtraffic_summary_updates.sh
+++ shell/summary_table_updates/logtraffic_summary_updates.sh
@@ -37,7 +37,7 @@ echo "Updating partitioned log traffic summary table with date range: $dateRange
 echo " "
 
 envs="production staging"
-appNames="persistJob json2trace parserJob coordinator ocrJob docreceiver careopt dataCheckAndRecover"
+appNames="persistJob json2trace parserJob coordinator ocrJob docreceiver careopt dataCheckAndRecover dataorchestrator_acl dataorchestrator_lookup dataorchestrator_request"
 
 for env in $envs
 do
