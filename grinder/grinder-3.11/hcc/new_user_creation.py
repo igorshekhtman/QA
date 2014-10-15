b3beeec89522cd6c3e97d209faff7c1b914f1445
Igor Shekhtman <ishekhtman@apixio.com>
2014-10-15 13:55:52 -0700

    added more features cleaning up url structure

--- grinder/grinder-3.11/hcc/new_user_creation.py
+++ grinder/grinder-3.11/hcc/new_user_creation.py
@@ -1,3 +1,24 @@
+#=========================================================================================
+# Author: Igor Shekhtman
+# Date Created: 15-Oct-2014
+# Initial Version: 1.0.0
+# Global Paramaters:
+# ENVIRONMENT - "Staging" or "Production"
+# NUMBEROFUSERSTOCREATE - integer - total number of HCC users to create
+#=========================================================================================
+# Revision 1:
+# Author:
+# Specifics:
+#=========================================================================================
+# Revision 2:
+# Author:
+# Specifics:
+#=========================================================================================
+# Revision 3:
+# Author:
+# Specifics:
+#=========================================================================================
+
 from net.grinder.script.Grinder import grinder
 from net.grinder.script import Test
 from net.grinder.plugin.http import HTTPRequest, HTTPPluginControl
@@ -7,73 +28,59 @@ from jarray import zeros
 import time
 import csv
 
+#===================== Global Test Environment Selection =================================
+#ENVIRONMENT = 'Production'
+ENVIRONMENT = 'Staging'
 
-#===================== Global Test Environment Selection ========================
-#environment = 'Production'
-environment = 'Staging'
+NUMBEROFUSERSTOCREATE = 2
+#=========================================================================================
 
-numberofusers = 1
-testduration = '10 seconds'
-#================================================================================
+#============ Global variable declaration, initialization ================================
 
-#============ Variable declaration, initialization ==============================
-userdatafn = "user_credentials-prd.csv"
-patientdatafn = "patient_uuids-prd.csv"
-documentdatafn = "document_uuids-prd.csv"
-useraccount = "useraccount-prd.apixio.com"
-tokenizer = "tokenizer-prd.apixio.com"
-dataorchestrator = "dataorchestrator-prd.apixio.com"
-postfix = "-prd"
-
-if (environment == "Production"):
-	postfix = "-prd"
+if (ENVIRONMENT == "Production"):
+	aclpostfix = "-prd"
+	hccpostfix = ""
 else:
-	postfix = "-stg"
+	aclpostfix = "-stg"
+	hccpostfix = "stage"
 
-
-userdatafn = "user_credentials" + postfix + ".csv"
-patientdatafn = "patient_uuids" + postfix + ".csv"
-documentdatafn = "document_uuids" + postfix + ".csv"
-
-acladmin = "acladmin" + postfix + ".apixio.com"
-useraccount = "useraccount" + postfix + ".apixio.com"
-tokenizer = "tokenizer" + postfix + ".apixio.com"
-dataorchestrator = "dataorchestrator" + postfix + ".apixio.com"
-
-auth = "https://" + useraccount + ":7076/auths"
-token = "https://" + tokenizer + ":7075/tokens"
-document = "https://" + dataorchestrator + ":7085/document"
-patient = dataorchestrator + ":7085/patient"
-event = dataorchestrator + ":7085/events"
-util = dataorchestrator + ":7085/util"
-
-thinkTime = "6 seconds"
 ok = 200
 created = 201
 accepted = 202
+nocontent = 204
 movedperm = 301
 redirect = 302
+forbidden = 403
+intserveror = 500
+servunavail = 503
 
 
-HOST_DOMAIN = "acladmin" +postfix+ ".apixio.com"
-HOST_URL = "https://" + HOST_DOMAIN
-#HOST_URL = "https://%s" % HOST_DOMAIN
-#Anthonys computer - used for testing
-#HOST_URL = "http://10.19.220.51:8086"
-USERNAME = "root@api.apixio.com"
-PASSWORD = "thePassword"
+ACL_PROTOCOL = "https://"
+ACL_DOMAIN = "acladmin" +aclpostfix+ ".apixio.com"
+ACL_URL = ACL_PROTOCOL + ACL_DOMAIN
+
+HCC_PROTOCOL = "https://"
+HCC_DOMAIN = "hcc" +hccpostfix+ ".apixio.com"
+HCC_URL = ACL_PROTOCOL + HCC_DOMAIN
+
+
+ACLUSERNAME = "root@api.apixio.com"
+ACLPASSWORD = "thePassword"
 MAX_OPPS = 2
-NEWUSER  = "apxdemot1010@apixio.net"
-USERPASSWORD = "apixio.123"
 USR_UUID = ""
 # Apixio Coders
 # ORG_UUID = "UO_059c7bbd-7ecc-4172-8d81-6ea2dadb6e76"
 # Load Test Coders
 ORG_UUID = "UO_149af107-1ef7-49a0-923e-be4b2de174b3"
+TOKEN = ""
+HCCUSERNAMEPREFIX = "test"
+HCCUSERNAMEPOSTFIX = "@apixio.net"
 HCCUSERNAME = ""
 HCCPASSWORD = "apixio.123"
-TOKEN = ""
+HCCUSERSLIST = [0]
 
+
+#=========================================================================================
 
 def create_request(test, headers=None):
     request = HTTPRequest()
@@ -111,6 +118,12 @@ def print_all_cookies(thread_context):
 def log(text):
     grinder.logger.info(text)
     print(text)
+    
+def get_new_hcc_user():
+	global HCCUSERNAMEPREFIX, HCCUSERNAMEPOSTFIX
+	hccusernumber = str(int(time.time()))
+	hccusername = HCCUSERNAMEPREFIX + hccusernumber + HCCUSERNAMEPOSTFIX
+	return hccusername	
 
 
 class TestRunner:
@@ -120,96 +133,83 @@ class TestRunner:
 		control = HTTPPluginControl.getConnectionDefaults()
 		control.setFollowRedirects(1)
 	
-		print "\nEnvironment: \t\t\t"+environment
-		print "User Account API URL: \t\t"+useraccount
-		print "Tokenizer API URL: \t\t"+tokenizer
-		print "Data Orchestrator API URL: \t"+dataorchestrator
-		print "Host URL: \t\t\t"+HOST_URL
-		print "Host Domain: \t\t\t"+HOST_DOMAIN+"\n\n"
+		print "\nEnvironment: \t\t\t"+ENVIRONMENT
+		#print "User Account API URL: \t\t"+useraccount
+		#print "Tokenizer API URL: \t\t"+tokenizer
+		#print "Data Orchestrator API URL: \t"+dataorchestrator
+		#print "Host URL: \t\t\t"+HOST_URL
+		#print "Host Domain: \t\t\t"+HOST_DOMAIN+"\n\n"
 		
-		#=================================================================================
+#=========================================================================================
 		def ACLObtainAuthorization():
-			global TOKEN		
+			global TOKEN, ACL_URL		
 			print "\nACL Obtain Authorization..."
-			print "HOST_URL: " + HOST_URL
+			#print "HOST_URL: " + HOST_URL
 			statuscode = 500
 			# repeat until successful login is reached
 			while statuscode != 200:
 		
 				login = create_request(Test(1000, 'ACL Log in admin'),[
-            		NVPair('Referer', 'https://acladmin-stg.apixio.com/'),
+            		NVPair('Referer', ACL_URL+'/'),
         		])
 			
-				result = login.POST(HOST_URL+"/auth", (
+				result = login.POST(ACL_URL+"/auth", (
 					NVPair('session', get_session(thread_context)),
-					NVPair('email', USERNAME),
-					NVPair('password', PASSWORD),))
+					NVPair('email', ACLUSERNAME),
+					NVPair('password', ACLPASSWORD),))
 		
 				TOKEN = get_session(thread_context)
 
 				statuscode = result.statusCode
 				print "Status Code = [%s]\t\t" % statuscode	
-		#=================================================================================
-		def ACLSelectUsers():
-			global TOKEN
-			print "\nACL Select useres tab..."
-			login = create_request(Test(2000, 'ACL Select users'),[
-            	NVPair('Referer', 'https://acladmin-stg.apixio.com/admin/'),
+#=========================================================================================
+		def ACLCreateNewUser():
+			global USR_UUID, HCCUSERNAME, TOKEN, ACL_URL
+			print "\nACL Create New User..."
+			login = create_request(Test(1100, 'ACL Create new user'),[
+    	        NVPair('Referer', ACL_URL+'/admin/'),
         	])
-			result = login.GET(HOST_URL+"/access/users", (
-				NVPair('session', TOKEN),))	
+
+			#HCCUSERNAME = "apxdemot00004@apixio.net"
+			HCCUSERNAME = get_new_hcc_user()
+			#print "Username = [%s]" % HCCUSERNAME
+			result = login.POST(ACL_URL+"/access/user", (
+				NVPair('email', HCCUSERNAME),
+				NVPair('session', TOKEN),))
+				
+			userjson = JSONValue.parse(result.getText())
+			if userjson is not None:
+				USR_UUID = userjson.get("id")
+				#print "User UUID: " + USR_UUID
+			
 			statuscode = result.statusCode
 			print "Status Code = [%s]\t\t" % statuscode
-		#=================================================================================
-		def ACLCreateNewUser():
-			global USR_UUID, HCCUSERNAME, TOKEN
-			print "\nACL Create New User..."
-			login = create_request(Test(3000, 'ACL Create new user'),[
-    	        NVPair('Referer', 'https://acladmin-stg.apixio.com/admin/'),
-        	])
-
-			# main loop, designating number of users to create
-			for i in range (0, 1):
-				HCCUSERNAME = "apxdemot000112"+str(i)+"@apixio.net"
-				print "Username = [%s]" % HCCUSERNAME
-				result = login.POST(HOST_URL+"/access/user", (
-					NVPair('email', HCCUSERNAME),
-					NVPair('session', TOKEN),))
-				
-				userjson = JSONValue.parse(result.getText())
-				#json2 = result.getText()
-				if userjson is not None:
-					USR_UUID = userjson.get("id")
-					print "User UUID: " + USR_UUID
-			
-				statuscode = result.statusCode
-				print "Status Code = [%s]\t\t" % statuscode
-				if statuscode == 500:
-					print ">>> Failure occured: username already exists <<<"
-					exit()
-		#=================================================================================
+			if statuscode == 500:
+				print ">>> Failure occured: username already exists <<<"
+				exit()
+#=========================================================================================
 		def ACLActivateNewUser():
-			global USR_UUID, TOKEN
+			global USR_UUID, TOKEN, ACL_URL
 			print "\nACL Activate New User..."	
-			print "User UUID: " + USR_UUID 
+			#print "User UUID: " + USR_UUID 
 			data = str.encode("session="+str(TOKEN))
-			login = create_request(Test(4000, 'ACL Activate new user'),[
-				NVPair('Referer', 'https://acladmin-stg.apixio.com/admin/'),
+			login = create_request(Test(1200, 'ACL Activate new user'),[
+				NVPair('Referer', ACL_URL+'/admin/'),
        	     ])
 
-			result = login.PUT(HOST_URL+"/access/user/"+USR_UUID, data, (
+			result = login.PUT(ACL_URL+"/access/user/"+USR_UUID, data, (
 				NVPair('session', TOKEN),))
 			
-			print_all_cookies(thread_context)
+			#print_all_cookies(thread_context)
 		
 			statuscode = result.statusCode
 			print "Status Code = [%s]\t\t" % statuscode		
-		#=================================================================================
+#=========================================================================================
 		def ACLSetPassword():
-			global USR_UUID, HCCPASSWORD, TOKEN
+			global USR_UUID, HCCPASSWORD, TOKEN, ACL_URL
 			print "\nACL Assign New User Password..."		
-			print "User UUID: " + USR_UUID 
-			print "Password: " + HCCPASSWORD
+			#print "User UUID: " + USR_UUID 
+			#print "Password: " + HCCPASSWORD
 
 			headers = [
     	        NVPair('Accept', 'application/json, text/plain, */*'),
@@ -219,13 +219,13 @@ class TestRunner:
     	        NVPair('Content-Length', '19'),
     	        NVPair('Content-Type', 'application/x-www-form-urlencoded'),
     	        NVPair('Host', 'acladmin-stg.apixio.com'),
-    	        NVPair('Origin', 'https://acladmin-stg.apixio.com'),
-    	        NVPair('Referer', 'https://acladmin-stg.apixio.com/admin/'),
+    	        NVPair('Origin', ACL_URL),
+    	        NVPair('Referer', ACL_URL+'/admin/'),
     	        NVPair('session', TOKEN),
     	    ]
-			login = create_request(Test(5000, 'ACL Set Password'),headers)
+			login = create_request(Test(1300, 'ACL Set Password'),headers)
 		
-			password_url = HOST_URL+"/access/user/"+USR_UUID+"/password"
+			password_url = ACL_URL+"/access/user/"+USR_UUID+"/password"
 			params = (NVPair('password', HCCPASSWORD),)
 			
 			#data = Codecs.mpFormDataEncode(params,zeros(1,NVPair),headers)
@@ -233,72 +233,87 @@ class TestRunner:
 			#result = login.PUT(HOST_URL+"/access/user/"+USR_UUID+"/password", 
 			#	data, headers)
 		
-			result = login.PUT(HOST_URL+"/access/user/"+USR_UUID+"/password", 
+			result = login.PUT(ACL_URL+"/access/user/"+USR_UUID+"/password", 
 				str.encode("password=apixio.123"), headers)
 					
 			statuscode = result.statusCode
 			print "Status Code = [%s]\t\t" % statuscode	
-		#=================================================================================
+#=========================================================================================
  		def ACLAssignCodingOrg():
- 			global USR_UUID, ORG_UUID
+ 			global USR_UUID, ORG_UUID, ACL_URL
 			print "\nACL Assign Coding Organization..."	
-			print "User UUID: " + USR_UUID 
-			print "Org UUID: " + ORG_UUID
+			#print "User UUID: " + USR_UUID 
+			#print "Org UUID: " + ORG_UUID
 			
-			login = create_request(Test(6000, 'ACL Add Coding Organization'),[
-	   	         NVPair('Referer', 'https://acladmin-stg.apixio.com/admin/'),
+			login = create_request(Test(1400, 'ACL Add Coding Organization'),[
+	   	         NVPair('Referer', ACL_URL+'/admin/'),
 	        ])
-			result = login.POST(HOST_URL+"/access/userOrganization/"+ORG_UUID+"/"+USR_UUID, (
+			result = login.POST(ACL_URL+"/access/userOrganization/"+ORG_UUID+"/"+USR_UUID, (
 				NVPair('session', TOKEN),))
 				
 			
 			statuscode = result.statusCode
 			print "Status Code = [%s]\t\t" % statuscode
-		#=================================================================================
+#=========================================================================================
 		def HCCLogInto():
-			global HCCUSERNAME, HCCPASSWORD
+			global HCCUSERNAME, HCCPASSWORD, HCC_URL
 			HCC_HOST_DOMAIN = 'hccstage.apixio.com'
 			HCC_HOST_URL = 'https://%s' % HCC_HOST_DOMAIN
 		
-			print "\nHCC Log into HCC with newly created user..."	
-			print "HCC Host Domain: " + HCC_HOST_DOMAIN
-			print "HCC Host URL: " + HCC_HOST_URL
+			#print "\nHCC Log into HCC with newly created user..."	
+			#print "HCC Host Domain: " + HCC_HOST_DOMAIN
+			#print "HCC Host URL: " + HCC_HOST_URL
 		
-			print "\nConnecting to host..."
-			create_request(Test(7000, 'HCC Connect to host')).GET(HCC_HOST_URL + '/')
+			print "\nHCC Connecting to host..."
+			result = create_request(Test(2000, 'HCC Connect to host')) \
+				.GET(HCC_URL + '/')
+			statuscode = result.statusCode
+			print "Status Code = [%s]\t\t" % statuscode		
 		
-			print "\nDetecting login page..."
-			create_request(Test(7100, 'HCC Get login page')).GET(HCC_HOST_URL + '/account/login/?next=/')
+			print "\nHCC Detecting login page..."
+			result = create_request(Test(2100, 'HCC Get login page')) \
+				.GET(HCC_URL + '/account/login/?next=/')
+			statuscode = result.statusCode
+			print "Status Code = [%s]\t\t" % statuscode		
 		
 			# Create login request. Referer appears to be necessary
-			login = create_request(Test(7200, 'HCC Log in user'),[
-				NVPair('Referer', HCC_HOST_URL + '/account/login/?next=/'),
+			login = create_request(Test(2200, 'HCC Log in user'),[
+				NVPair('Referer', HCC_URL + '/account/login/?next=/'),
 			])
 		
 			print "\nLogging in to HCC Front End..."
-			result = login.POST(HCC_HOST_URL + '/account/login/?next=/', (
+			result = login.POST(HCC_URL + '/account/login/?next=/', (
 				NVPair('csrfmiddlewaretoken', get_csrf_token(thread_context)),
 				NVPair('username', HCCUSERNAME),
 				NVPair('password', HCCPASSWORD),))
 		
-			print HCCUSERNAME 
-			print HCCPASSWORD	
+			#print HCCUSERNAME 
+			#print HCCPASSWORD	
 			statuscode = result.statusCode
 			print "Status Code = [%s]\t\t" % statuscode	
-		#=================================================================================
-		#================== MAIN PROGRAM BODY ============================================
-		#=================================================================================
+#=========================================================================================
+#====================== MAIN PROGRAM BODY ================================================
+#=========================================================================================
+
+		for i in range (0, NUMBEROFUSERSTOCREATE):
+			ACLObtainAuthorization()
+			ACLCreateNewUser()
+			ACLActivateNewUser()
+			ACLSetPassword()
+			ACLAssignCodingOrg()
+			HCCLogInto()
+			
+			HCCUSERSLIST.append(i)
+			HCCUSERSLIST[i] = HCCUSERNAME
 		
-		ACLObtainAuthorization()
-		ACLSelectUsers()
-		ACLCreateNewUser()
-		ACLActivateNewUser()
-		ACLSetPassword()
-		ACLAssignCodingOrg()
-		HCCLogInto()
-		
+		print "\n================================"
+		print "List of newly created HCC Users:"
+		print "================================"
+		for i in range (0, NUMBEROFUSERSTOCREATE):
+			print HCCUSERSLIST[i]
+		print "================================"	
 		print "\nThe End..."
-		#=================================================================================
+#=========================================================================================
 		
 		
 		
