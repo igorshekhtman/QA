4cd2d05d8f42416867b810aff03ab3cc3780a5ec
Igor Shekhtman <ishekhtman@apixio.com>
2014-11-12 15:12:03 -0800

    code cleanup and general improvement

--- python/data_integrity_project/cassandra_trace_qa.py
+++ python/data_integrity_project/cassandra_trace_qa.py
@@ -3,8 +3,9 @@ import logging
 import os
 import json
 
-ORGID = "381"
+ORGID = "382"
 
+#"d12240a0-a67b-4fd6-969a-e17bcb3a022a"
 
 os.system('clear')
 log = logging.getLogger()
@@ -14,24 +15,25 @@ log.setLevel('INFO')
 cluster = Cluster(['10.199.22.32', '10.196.81.90', '10.198.2.83', '10.196.100.53', '10.197.91.36', '10.199.52.19'], port=9042)
 session = cluster.connect('apixio')
 session_new = cluster.connect('apixio')
-DE = ATSF = EJ = OJ = PAJ = PEJ = STC = 0
+
+PROC_MAP = { \
+	"docEntry":"DE", \
+	"appendToSequenceFile":"ATSF", \
+	"eventJob":"EJ", \
+	"ocrJob":"OJ", \
+	"parserJob":"PAJ", \
+	"persistJob":"PEJ", \
+	"submitToCoordinator":"STC", \
+}
+
+# Initialize all global counters to zero
+for i in PROC_MAP:
+	exec("%s = %d" % (PROC_MAP[i],0)) in globals()	
 
 def countNumberOfOccurrences(proc_name):
-	global DE, ATSF, EJ, OJ, PAJ, PEJ, STC
-	if proc_name == "docEntry":
-		DE = DE + 1
-	elif  proc_name == "appendToSequenceFile":
-		ATSF = ATSF + 1
-	elif  proc_name == "eventJob":
-		EJ = EJ + 1
-	elif  proc_name == "ocrJob":
-		OJ = OJ + 1
-	elif  proc_name == "parserJob":
-		PAJ = PAJ + 1
-	elif  proc_name == "persistJob":
-		PEJ = PEJ + 1
-	elif  proc_name == "submitToCoordinator":
-		STC = STC + 1
+	if proc_name in PROC_MAP:
+		exec("%s = %s + %d" % (PROC_MAP[proc_name],PROC_MAP[proc_name],1)) in globals()
+
 
 def checkForExistance (doc_uuid):
 	results_new = session_new.execute("""SELECT * FROM trace"""+ORGID+""" where rowkey='"""+doc_uuid+"""';""")
@@ -41,8 +43,9 @@ def checkForExistance (doc_uuid):
 			process_name = "docEntry"	
 		else:
 			process_name = str(data_new["processName"])
-		countNumberOfOccurrences(process_name)	
-		
+		countNumberOfOccurrences(process_name)
+		#if process_name == "docEntry":	
+		#	print str(row_new)
 		#print "\n" + str(data_new["documentUUID"]) + "\t" + process_name	
 
 
@@ -67,19 +70,17 @@ for row in results:
 		print "================\n"
 
 print "\n\n\n"
-print "Total number of documents received from Doc-Receiver: " + str(counter)
-print "Total number of documents docEntry: " + str(DE)
-print "Total number of documents appendToSequenceFile: " + str(ATSF)
-print "Total number of documents eventJob: " + str(EJ)
-print "Total number of documents ocrJob: " + str(OJ)
-print "Total number of documents parserJob: " + str(PAJ)
-print "Total number of documents persistJob: " + str(PEJ)
-print "Total number of documents submitToCoordinator: " + str(STC)
+print "received from Doc-Receiver: " + str(counter)
+print "\n"
+
+for i in PROC_MAP:
+	print str(i) +  ": " + str(eval(PROC_MAP[i]))
+
 print "\n\n"
 
 
 
-#checkForExistance("docuuid_c02101c0-9dde-4c1d-9cc0-2673971414dd")
+#checkForExistance("docuuid_d12240a0-a67b-4fd6-969a-e17bcb3a022a")
 
 
 cluster.shutdown()
