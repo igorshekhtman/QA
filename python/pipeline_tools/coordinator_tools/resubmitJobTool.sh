b344e9319ca135338d7f5a1291c44fe83f0474dc
Igor Shekhtman <ishekhtman@apixio.com>
2015-03-17 16:08:54 -0700

    initial version of the shell to python converted pipeline-tools

---
+++ python/pipeline_tools/coordinator_tools/resubmitJobTool.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+#
+# lschneider
+
+#  Usage:
+#   job <jobId>
+#   activity <activity>
+#   type <type>
+#   org <orgId>
+#   all
+#
+#   ignore job <jobId>
+#   ignore activity <activity>
+#   ignore type <type>
+#   ignore org <orgId>
+#
+#   list (default)
+#
+#  Note:
+#   Only accepts one argument at a time.
+#   types = {failed, orphaned, launcherror}
+
+if [ "$1" = "help" ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]
+then
+    echo "
+======== Usage ========
+    job <jobId>
+    activity <activity>
+    type <type>
+    org <orgId>
+    all
+
+    ignore job <jobId>
+    ignore activity <activity>
+    ignore type <type>
+    ignore org <orgId>
+
+    list (default)
+    "
+    exit
+fi
+
+. /usr/lib/apx-coordinator/bin/setcp
+
+if [ "$1" = "job" ]
+then
+    $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --submit --job "$2"
+elif [ "$1" = "activity" ]
+then
+    $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --submit --activity $2
+elif [ "$1" = "type" ]
+then
+    $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --submit --type $2
+elif [ "$1" = "org" ]
+then
+    $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --submit --org "$2"
+elif [ "$1" = "all" ]
+then
+    $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --submit --all
+elif [ "$1" = "ignore" ]
+then
+    if [ "$2" = "job" ]
+    then
+        $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --ignore --job "$3"
+    elif [ "$2" = "activity" ]
+    then
+        $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --ignore --activity $3
+    elif [ "$2" = "type" ]
+    then
+        $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --ignore --type $3
+    elif [ "$2" = "org" ]
+    then
+        $JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --ignore --org "$3"
+    fi
+fi
+
+$JAVAEXEC com.apixio.coordinator.cmdline.ResubmitJob --list
+
