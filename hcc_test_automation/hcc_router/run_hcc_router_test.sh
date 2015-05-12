36f776d7cfb7f571b251498d4bb7f91224653b1b
Igor Shekhtman <ishekhtman@apixio.com>
2015-05-12 12:33:12 -0700

    continuing integration of router tests into apixio automated test framework

---
+++ hcc_test_automation/hcc_router/run_hcc_router_test.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+#
+# Copyright 2015 Apixio
+#
+# Author: Igor Shekhtman
+# Email:  ishekhtman@apixio.com
+# Date:   05/12/2015
+#
+#
+
+echo "Starting Nose Test..."
+
+epoch_date1=$(date +%s)
+epoch_date2=${epoch_date1:0:6}
+
+year=$(date +%Y)
+month=$(date +%m)
+day=$(date +%d)
+echo year=$year month=$month day=$day
+
+#echo $1
+
+if [ -z "$1" ]
+  then
+    echo "No specific test selected, terminating ..."
+    exit 1
+  else
+  	specific_test=$1
+    echo "Running $test_number test(s) ..." 
+fi
+
+#exit 0
+
+
+
+echo "Creating folders if do not already exist..."
+# work folder
+mkdir -p /usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month
+# backup folder
+mkdir -p /mnt/reports/staging/hccrouterregression/$year/$month
+echo "Completed creating new folders..."
+
+tests_folder="/mnt/hcc_test_automation/hcc_router/"
+
+
+if [ "$specific_test" = "all" ];
+	then
+		nosetests --with-xunit --xunit-file=/usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month/$day.xml --where=$tests_folder	--all-modules
+	else
+		nosetests --with-xunit --xunit-file=/usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month/$day.xml --tests=/mnt/hcc_test_automation/hcc_router/$specific_test.py	
+fi;	
+
+xsltproc style.xsl /usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month/$day.xml > /usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month/$day.html
+
+# backup to archive folder
+cp -avr /usr/lib/apx-reporting/assets/reports/staging/hccrouterregression/$year/$month/$day.* /mnt/reports/staging/hccrouterregression/$year/$month/
+
+# Check if the report line item already exists in txt file or not
+# Append if does not, skip if does
+directory="/usr/lib/apx-reporting/assets/"
+file="hcc_hccrouterregression_reports_staging.txt"
+lineitem="reports/staging/hccrouterregression/$year/$month/$day.html"
+#echo directory=$directory
+#echo file=$file
+#echo item=$lineitem
+
+cd $directory  
+if grep -q "$lineitem" "$file";
+then
+echo "Report entry already exists, skipping write ..."
+else
+echo "Appending new report entry line to the $File ..."
+echo -n -e "HCC Regression Staging Report - "$(date +"%B %d, %Y")"\treports/staging/hccrouterregression/$year/$month/$day.html\n" >> /usr/lib/apx-reporting/assets/hcc_hccrouterregression_reports_staging.txt
+fi
+
+echo "Nose Test Completed..."
