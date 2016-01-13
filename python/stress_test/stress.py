0e113783271a9905a42b20b2e2a8de6962025214
Igor Shekhtman <ishekhtman@apixio.com>
2016-01-13 15:06:54 -0800

    fixed issue with coding delay time entered as command line paramater char vs integer issue

--- python/stress_test/stress.py
+++ python/stress_test/stress.py
@@ -667,8 +667,12 @@ def Main():
   reject = int(sys.argv[9]) if len(sys.argv) > 9 else 45
   skip = int(sys.argv[10]) if len(sys.argv) > 10 else 10
   options['action_weights'] = {'view':0,'accept':accept,'reject':reject,'skip':skip}
-  options['dos'] = sys.argv[11] if len(sys.argv) > 11 else "04/04/2014"
-  options['report_recepients'] = sys.argv[12] if len(sys.argv) > 12 else "ishekhtman@apixio.com"
+  options['dos'] = str(sys.argv[11]) if len(sys.argv) > 11 else "04/04/2014"
+  options['report_recepients'] = str(sys.argv[12]) if len(sys.argv) > 12 else "ishekhtman@apixio.com"
+
+  print options['dos']
+  print options['report_recepients']
+  quit()
 
 
   defineGlobals()
